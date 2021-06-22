6297847b314f40aebf96519749b1f850f6d90849
AGRTECH <62949445+AGRTECH@users.noreply.github.com>
2021-06-21 21:14:25 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const userInput = document.querySelector("input");
+const submitBtn = document.querySelector(".submit");
+const todoItem = document.querySelector(".todoitem");
+const container = document.querySelector(".container-2");
+const removeX = document.querySelectorAll(".fa-times");
+
+submitBtn.addEventListener("click", () => {
+  let newTaskDiv = document.createElement("div");
+  let iconHolder = document.createElement("i");
+  newTaskDiv.innerHTML = userInput.value;
+  newTaskDiv.className = "todoitem";
+  iconHolder.className = "fas fa-times";
+  newTaskDiv.appendChild(iconHolder);
+  container.appendChild(newTaskDiv);
+  userInput.value = "";
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.className === "fas fa-times") {
+    e.target.parentElement.remove();
+  }
+});
